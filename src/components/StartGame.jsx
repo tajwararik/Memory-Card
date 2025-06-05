7b00a03ed9bdf365c94a52ff0441a93f30f18de6
tajwararik <tajwar.arik.10@gmail.com>
2025-06-05 20:11:08 +0600

    Created StartGame component

---
+++ src/components/StartGame.jsx
@@ -0,0 +1,7 @@
+import RegularButton from "./RegularButton";
+
+function StartGame({ handleClick }) {
+  return <RegularButton handleClick={handleClick}>Start game</RegularButton>;
+}
+
+export default StartGame;
