f02b4e8c5029e183364b03e8b14321df1b8252d1
tajwararik <tajwar.arik.10@gmail.com>
2025-06-05 20:19:06 +0600

    Made start game button functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import StartGame from "./components/StartGame";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [isGameOn, setIsGameOn] = useState(false);
+
+  function startGame() {
+    setIsGameOn(true);
+  }
+
+  return (
+    <>
+      <h1>Memory Card</h1>
+      {!isGameOn && <StartGame handleClick={startGame} />}
+    </>
+  );
 }
 
 export default App;
