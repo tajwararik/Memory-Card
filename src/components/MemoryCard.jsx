ffb32294c67830d92ca03800dfe3a2e07f4aac4f
tajwararik <tajwar.arik.10@gmail.com>
2025-06-15 18:19:29 +0600

    Created MemoryCard component

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+import EmojiButton from "./EmojiButton";
+
+function MemoryCard({ cards }) {
+  const card = cards.map((emoji, index) => {
+    return (
+      <li key={index} className="cards">
+        <EmojiButton emojiData={emoji} />
+      </li>
+    );
+  });
+
+  return <ul className="card-container">{card}</ul>;
+}
+
+export default MemoryCard;
