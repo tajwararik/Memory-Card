539efea186120ccd72ae2f76898a176812989157
tajwararik <tajwar.arik.10@gmail.com>
2025-06-15 18:20:28 +0600

    Created EmojiButton component

---
+++ src/components/EmojiButton.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "../styles/style.css";
+
+function EmojiButton({ emojiData }) {
+  const [emojiURL, setEmojiURL] = useState("");
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const response = await fetch(emojiData.url);
+        const data = await response.json();
+
+        setEmojiURL(data.sprites.front_default);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    fetchPokemon();
+  }, [emojiData.url]);
+
+  return (
+    <div className="emoji-button">
+      <div className="image-container">
+        {emojiURL && <img src={emojiURL} alt={emojiData.name} />}
+      </div>
+      <p>{emojiData.name}</p>
+    </div>
+  );
+}
+
+export default EmojiButton;
